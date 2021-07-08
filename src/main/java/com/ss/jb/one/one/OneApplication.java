83a6ebcd15700d3fc41b8b97207733aa4501a70a
murphcoder <murphcoder@gmail.com>
2021-07-07 20:02:08 -0500

    first draft

--- src/main/java/com/ss/jb/one/one/OneApplication.java
+++ src/main/java/com/ss/jb/one/one/OneApplication.java
@@ -1,13 +1,47 @@
 package com.ss.jb.one.one;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class OneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OneApplication.class, args);
+		System.out.println("1)");
+		for (int i = 1; i <= 4; i++) {
+			printLine(i, "*");
+		};
+		printLine(9, ".");
+		System.out.println("2)");
+		printLine(10, ".");
+		for (int i = 4; i > 0; i--) {
+			printLine(i, "*");
+		};
+		System.out.println("3)");
+		int starCount = 1;
+		for (int i = 5; i > 1; i--) {
+			printIndentLine(i);
+			printLine(starCount, "*");
+			starCount = starCount + 2;
+		};
+		printLine(11, ".");
+		System.out.println("4)");
+		printLine(12, ".");
+		starCount = 7;
+		for (int i = 2; i < 6; i++) {
+			printIndentLine(i);
+			printLine(starCount, "*");
+			starCount = starCount - 2;
+		};
+	};
+
+	static void printIndentLine(int Count) {
+		for (int i = 0; i < Count; i++) {
+			System.out.print(" ");
+		};
+	};
+
+	static void printLine(int Count, String Char) {
+		for (int i = 0; i < Count; i++) {
+			System.out.print(Char);
+		};
+		System.out.println("");
 	}
 
 }
